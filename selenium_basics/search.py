5addf326a122c52de9d9929d586fe28d04456a61
OlgaLesser <lesserolga@gmail.com>
2025-01-28 09:58:29 -0700

    Update search.py

--- selenium_basics/search.py
+++ selenium_basics/search.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Chrome()
-driver.get("http://www.ebay.com")
-wait = WebDriverWait(driver, 10)
-element = wait.until(EC.element_to_be_clickable((By.ID, "gh-ac")))
+driver.get("https://www.ebay.com")
+search_field = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, "//input[@id='gh-ac']")))
 print(driver.current_url)
-element.send_keys("women watch")
-search = driver.find_element(By.ID, "gh-btn")
-search.click()
-header = driver.find_element(By.CLASS_NAME, "srp-controls__count-heading")
-assert "830,000+ results for women watch" in header.text
+search_field.send_keys("women watch")
+search_button = driver.find_element(By.XPATH, "//button[@id='gh-search-btn']")
+search_button.click()
+header_element = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, "//h1[@class='srp"
+                                                                                           "-controls__count-heading']")))
+assert "results for women watch" in header_element.text
 driver.quit()
